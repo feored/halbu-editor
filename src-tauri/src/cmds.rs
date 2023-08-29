55ea3d8e1359cbbbd4c74ff866a8e31833470fcd
feored <feordo@gmail.com>
2023-08-30 01:31:56 +0200

    Bugfix checking all .d2s files in folder.

--- src-tauri/src/cmds.rs
+++ src-tauri/src/cmds.rs
@@ -1,10 +1,10 @@
+use halbu::Save;
 use serde::{Deserialize, Serialize};
+use std::ffi::OsStr;
 use std::fs::read_dir;
 use std::fs::OpenOptions;
 use std::io::Write;
 use std::path::Path;
-
-use halbu::Save;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct SaveFile {
@@ -17,7 +17,7 @@ pub fn get_character_from_path(path: String) -> Result<Save, String> {
     let path: &Path = Path::new(&path);
     let save_file: Vec<u8> = match std::fs::read(path) {
         Ok(bytes) => bytes,
-        Err(e) => panic!("File invalid: {e:?}"),
+        Err(e) => return Err(e.to_string()),
     };
 
     let result = match halbu::parse(&save_file) {
@@ -50,21 +50,21 @@ pub fn save_file(path: String, save: Save) -> Result<String, String> {
 #[tauri::command]
 pub fn summary_folder(path: String) -> Result<Vec<SaveFile>, String> {
     let path: &Path = Path::new(&path);
+
     let files = match read_dir(path) {
         Ok(res) => res,
         Err(e) => return Err(e.to_string()),
     };
+
     let mut saves: Vec<SaveFile> = Vec::<SaveFile>::new();
     for file in files {
-        let file = match file {
-            Ok(res) => res,
+        let file_path = match file {
+            Ok(res) => res.path(),
             Err(_e) => continue,
         };
 
-        let file_path = file.path();
-        match file_path.as_path().extension() {
-            Some(res) => res.to_str() == Some("d2s"),
-            None => continue,
+        if file_path.as_path().extension() != Some(OsStr::new("d2s")) {
+            continue;
         };
 
         let file_path_string = match file_path.as_path().to_str() {
